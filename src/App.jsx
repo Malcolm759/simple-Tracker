206a12ff74b76cf4507f3069ca8aa0d1e1ae01af
Malcolm_x <malcolmekene94@gmail.com>
2026-09-22 11:46:12 +0100

    Another change

--- src/App.jsx
+++ src/App.jsx
@@ -362,7 +362,7 @@ const App = () => {
       .from("transactions")
       .select("*")
       .order("created_at", { ascending: false })
-      .limit(10);
+      .limit(25);
     if (!error) setTxs(data || []);
   };
 
